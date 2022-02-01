4400605a20a77205746e005f418348e81e00d5b7
AngeloDalo <angelo251158@gmail.com>
2022-02-01 15:17:47 +0100

    header

--- index.php
+++ index.php
@@ -79,14 +79,42 @@ $array_paragrafo = [];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@400;700&family=Libre+Baskerville:ital@1&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">
     <title>GOOGLE</title>
 </head>
 <body>
     <header>
-
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-8">
+                    <div class="row">
+                        <div class="col-12">
+                            <img class="google-logo" src="img\google-logo-attuale-scritta-multicolore.png" alt="google-logo">
+                            <a id="privacy" href="#">Privacy e termini</a>
+                        </div>
+                    </div>
+                    <div class="row link-row">
+                        <div class="col-12">
+                            <a href="#">Introduzione</a>
+                            <a href="#">Norme sulla privacy</a>
+                            <a href="#">Termini di servizio</a>
+                            <a href="#">Tecnologie</a>
+                            <a href="#">Domande frequenti</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 d-flex justify-content-end align-items-center mb-5">
+                    <i class="fas fa-bars"></i>
+                    <div class="avatar">
+                        <img src="img\avatar.jpg" alt="avatar.jpg">
+                    </div>
+                </div>
+            </div>
+        </div>
     </header>
     <main>
         <div class="div">
